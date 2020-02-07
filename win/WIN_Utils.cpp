5cbec05e02d7e3e4ba4ef63017c7c64f8febb827
Zoe <zoe.vallis@vivid-q.com>
2020-02-07 15:58:56 +0000

    calculating outcode function
    for Cohen-Sutherland line clipping algorithm

--- win/WIN_Utils.cpp
+++ win/WIN_Utils.cpp
@@ -1,5 +1,6 @@
 #include "WIN_pch.h"
 #include "WIN_Utils.h"
+#include "GFX_Rectangle.h"
 
 using namespace win;
 using namespace utils;
@@ -11,4 +12,40 @@ bool win::utils::filterNumerical(const char c)
 	}
 	
 	return false;
+}
+
+//TODO: will need to test extensively, to check border cases
+// Outcode for draw window clipping
+// note we are flipping 'above' and 'below' the rectangle, due to how coordinates work
+/*	INSIDE		0000 -> 0
+ *	LEFT		0001 -> 1
+ *	RIGHT		0010 -> 2
+ *	BELOW		0100 -> 4
+ *	ABOVE		1000 -> 8
+ *	
+ *	ABOVE-LEFT	1001 -> 9
+ *	BELOW-LEFT	0101 -> 5
+ *	ABOVE-RIGHT	1010 -> 10
+ *	BELOW-RIGHT	0110 -> 6
+ */
+int win::utils::findOutcode(const gfx::Rectangle rect, const int x, const int y)
+{
+	//Initialise outcode as inside the rectangle
+	int outcode = 0;
+
+	if (x < rect.x) {
+		outcode |= 1;
+	}
+	else if (x > rect.x + rect.width) {
+		outcode |= 2;
+	}
+
+	if (y < rect.y) {
+		outcode |= 8;
+	}
+	else if (y > rect.y + rect.height) {
+		outcode |= 4;
+	}
+
+	return outcode;
 }
